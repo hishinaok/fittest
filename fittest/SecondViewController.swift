336b541e6f830330457a5cffcc9d39a850055642
Naoko Hishinuma <NAOKO@naoko-no-macbook.local>
2016-12-07 17:15:48 +0900

    Initial Commit

---
+++ fittest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fittest
+//
+//  Created by Naoko Hishinuma on 2016/12/07.
+//  Copyright © 2016年 Naoko Hishinuma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
